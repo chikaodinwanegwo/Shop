8232887987dbeb1f2aa5bf9bbaee4d2830e2ed63
chikaodinwanegwo <63511232+chikaodinwanegwo@users.noreply.github.com>
2025-07-23 16:32:14 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,10 @@
+will eventually fetch from backend servers (mockbackend/data)
+*/
+
+export default {
+  "/categories": ["Shirts", "Pants", "Shoes", "Accessories"],
+  "/items?category=Shirts": ["T-Shirts", "Casual", "Formal"],
+  "/items?category=Pants": ["Long Pants", "Sweat Pants", "Shorts", "Swimwear"],
+  "/items?category=Shoes": ["Athletic", "Professional", "Casual", "Walking"],
+  "/items?category=Accessories": ["Hats", "Wallets", "Belts"]
+};
